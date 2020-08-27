6f884ff129f53dba720aa55bb04f541dab73fc0a
paolinor88 <68273270+paolinor88@users.noreply.github.com>
2020-08-27 18:40:17 +0200

    minor update

--- magazzino/request.php
+++ magazzino/request.php
@@ -37,38 +37,120 @@ if (($_SESSION["ID"])!='D9999'){
             <li class="breadcrumb-item active" aria-current="page">Richiesta materiale</li>
         </ol>
     </nav>
-    <div class="card text-center border-danger">
-        <div class="card-header text-danger">
-            OOOPS!
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">PAGINA IN COSTRUZIONE</h5>
-            <p class="card-text">Spiacenti, la risorsa che stai cercando non è ancora disponibile</p>
-        </div>
-        <div class="card-footer text-muted">
-            Se riscontri un errore, <a href="mailto:gestioneutenti@croceverde.org">contatta il webmaster</a>
-        </div>
-    </div>
-    <div class="jumbotron">
-        <?php
+    <form action="request.php" method="post">
+        <div class="accordion" id="accordionExample">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Materiale di consumo
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <?php
 
-        $select = $db->query("SELECT * FROM giacenza WHERE categoria='1' order by nome");
+                        $select = $db->query("SELECT DISTINCT nome, tipo FROM giacenza WHERE categoria='1' order by nome, tipo");
 
-        while($ciclo = $select->fetch_array()){
+                        while($ciclo = $select->fetch_array()){
 
-                echo "
-					<div class=\"form-group form-check\">
-                        <input type=\"checkbox\" class=\"form-check-input\" id=\"".$ciclo['nome']." ".$ciclo['tipo']."\">
-                        <label class=\"form-check-label\" for=\"".$ciclo['nome']." ".$ciclo['tipo']."\">".$ciclo['nome']." ".$ciclo['tipo']."</label>
+                            echo "
+                                    <div class=\"form-group row\">
+                                        <div class=\"col-sm-1\" >
+                                           <input type=\"text\" class=\"form-control form-control-sm oggetto\" name=\"".$ciclo['nome']." ".$ciclo['tipo']."\" value=''>
+                                        </div>
+                                        <label class=\"col-sm-11 col-form-label\" for=\"".$ciclo['nome']." ".$ciclo['tipo']."\">".$ciclo['nome']." ".$ciclo['tipo']."</label>
+                                    </div>
+                                    ";
+
+                        }
+
+                        ?>
                     </div>
-					";
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingTwo">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Vestiario
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <?php
 
-        }
+                        $select = $db->query("SELECT DISTINCT nome, tipo FROM giacenza WHERE categoria='4' order by nome, tipo");
 
-        ?>
-    </div>
+                        while($ciclo = $select->fetch_array()){
+
+                            echo "
+                                    <div class=\"form-group row\">
+                                        <div class=\"col-sm-1\">
+                                           <input type=\"text\" class=\"form-control form-control-sm\" id=\"".$ciclo['nome']." ".$ciclo['tipo']."\">
+                                        </div>
+                                        <label class=\"col-sm-11 col-form-label\" for=\"".$ciclo['nome']." ".$ciclo['tipo']."\">".$ciclo['nome']." ".$ciclo['tipo']."</label>
+                                    </div>
+                                    ";
+
+                        }
+
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingThree">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Altro
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div class="form-group">
+                            <textarea class="form-control" name="note" id="note" rows="10" maxlength="250"></textarea>
+                            <span id="conteggio" style="font-size: small; color: grey"></span>
+                            <script type="text/javascript">
+                                // avvio il controllo all'evento keyup
+                                $('textarea#note').keyup(function() {
+                                    // definisco il limite massimo di caratteri
+                                    var limite = 250;
+                                    var quanti = $(this).val().length;
+                                    // mostro il conteggio in real-time
+                                    $('span#conteggio').html(quanti + ' / ' + limite);
+                                    // quando raggiungo il limite
+                                    if(quanti >= limite) {
+                                        // mostro un avviso
+                                        $('span#conteggio').html('<strong>Non puoi inserire più di ' + limite + ' caratteri!</strong>');
+                                        // taglio il contenuto per il numero massimo di caratteri ammessi
+                                        var $contenuto = $(this).val().substr(0,limite);
+                                        $('textarea#note').val($contenuto);
+                                    }
+                                });
+                            </script>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <center>
+            <button type="submit" id="inviarichiesta" name="inviarichiesta" class="btn btn-success"><i class="fas fa-check"></i></button>
+        </center>
+    </form>
 </div>
-
+<br>
+<?
+/*//ottieni items
+if(isset($_POST["inviarichiesta"])&&(($_POST[$ciclo['nome'].' '.$ciclo['tipo']])!="")){
+    echo "OK";
+}else{
+    echo "ERRORE";
+}*/?>
 
 </body>
 <?php include('../config/include/footer.php'); ?>
