f7d2037d68504aa19106a2c0b7c5a6f9b37ea141
paolinor88 <68273270+paolinor88@users.noreply.github.com>
2020-09-27 09:22:20 +0200

    favicon improvement

--- index.php
+++ index.php
@@ -45,6 +45,23 @@ $dictionaryLivello = array (
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <meta name="author" content="Paolo Randone">
+    <link rel="apple-touch-icon" sizes="57x57" href="config/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="config/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="config/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="config/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="config/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="config/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="config/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="config/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="config/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="config/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="config/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="config/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="config/favicon/favicon-16x16.png">
+    <link rel="manifest" href="config/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <title>Gestionale CV-TO - Home page</title>
 
     <link rel="stylesheet" href="config/css/bootstrap.min.css">
