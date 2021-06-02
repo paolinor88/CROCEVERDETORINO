53e3b1998559cfa283379487cca0c8abe338d242
paolinor88 <68273270+paolinor88@users.noreply.github.com>
2021-06-02 23:33:15 +0200

    Delete destinatari.php

--- config/include/destinatari.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$randone= 'mail@paolorandone.it';
-$bechis= 'massimilianobechis@gmail.com';
-$gestionale= 'gestioneutenti@croceverde.org';
-$comunicazioni= 'comunicazioni.mezzi@croceverde.org';
-$checklist='checklist@croceverde.org';
