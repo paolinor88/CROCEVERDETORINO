13c2fdfe4ff3c39dc806c1a66877bc2091b38750
Paolo Randone <mail@paolorandone.it>
2025-04-01 12:35:29 +0200

    .gitignore

---
+++ gestionale/config/include/destinatari.php
@@ -0,0 +1,10 @@
+<?php
+
+$randone= 'paolo.randone@croceverde.org';
+$bechis= 'massimiliano.bechis@croceverde.org';
+$gestionale= 'gestioneutenti@croceverde.org';
+$autoparco= 'autoparco@croceverde.org';
+$checklist='checklist@croceverde.org';
+$ufficioautoparco='ufficioautoparco@croceverde.org';
+
+$apiToken = "2028400512:AAHK_wHbZpOwsLYLBpu2V76rN1FUPL1N4Yo";
--- strumenti/config/pdo.php
+++
@@ -1,2 +0,0 @@
-<?php
-$connect=new PDO('mysql:host=localhost;dbname=massi369_gestionale', 'urhqx7h4kxv84', 'Gestional€');
